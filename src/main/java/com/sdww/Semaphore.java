d1f2a69fd37331fb632a9336314d08fc12ef95eb
sdww2348115 <2324717228@qq.com>
2018-03-13 22:28:55 +0800

    Semaphore

---
+++ src/main/java/com/sdww/Semaphore.java
@@ -0,0 +1,9 @@
+package com.sdww;
+
+/**
+ * Semaphore俗称信号量，是最基本的同步工具之一，最主要的作用是限制资源的总量/获取.
+ * Semaphore默认值为1，bitSemaphore，它的行为类似于lock，但是与lock最大的不同在于：它可以被owner之外的线程释放
+ * 其核心本质就是ASQ的封装
+ */
+public class Semaphore {
+}
