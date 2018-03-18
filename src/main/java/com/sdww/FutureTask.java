2e1bba4d01a9e755bbc0df1d89119266febb3e08
sdww2348115 <2324717228@qq.com>
2018-03-18 23:36:21 +0800

    FutureTask

--- src/main/java/com/sdww/FutureTask.java
+++ src/main/java/com/sdww/FutureTask.java
@@ -72,6 +72,7 @@ public class FutureTask {
              * 1.如果在set state之前就将runner清空，此时cancel方法被调用，线程未被interrupt，线程所经历的状态就变为了：
              * NEW -> INTERRUPTING -> NORMAL!这显然是非法的
              * 2.要保证Thread抛出interrupt异常，就必须要保证该task仍然在run的状态，所以如果处在INTERRUPTING状态时，需要yeild一段时间等待interrupt信号
+             * 如果interrupt信号在run后才发送到该线程，将进入threadPool中的其他逻辑，导致出现非预料的情况
              */
             // runner must be non-null until state is settled to
             // prevent concurrent calls to run()
@@ -155,6 +156,8 @@ public class FutureTask {
     /**
      * Ensures that any interrupt from a possible cancel(true) is only
      * delivered to a task while in run or runAndReset.
+     * 要保证Thread抛出interrupt异常，就必须要保证该task仍然在run的状态，所以如果处在INTERRUPTING状态时，需要yeild一段时间等待interrupt信号
+     * 如果interrupt信号在run后才发送到该线程，将进入threadPool中的其他逻辑，导致出现非预料的情况
      */
     private void handlePossibleCancellationInterrupt(int s) {
         // It is possible for our interrupter to stall before getting a
@@ -174,6 +177,11 @@ public class FutureTask {
         // Thread.interrupted();
     }
 
+    /**
+     * 这里需要注意的是，interrupt信号必须在task.run()的过程中发送给相应线程
+     * @param mayInterruptIfRunning
+     * @return
+     */
     public boolean cancel(boolean mayInterruptIfRunning) {
         if (!(state == NEW &&
                 UNSAFE.compareAndSwapInt(this, stateOffset, NEW,
@@ -193,5 +201,145 @@ public class FutureTask {
             finishCompletion();
         }
         return true;
+    }
+
+    /**
+     * @throws CancellationException {@inheritDoc}
+     */
+    public V get() throws InterruptedException, ExecutionException {
+        int s = state;
+        if (s <= COMPLETING)
+            s = awaitDone(false, 0L);
+        return report(s);
+    }
+
+    /**
+     * @throws CancellationException {@inheritDoc}
+     */
+    public V get(long timeout, TimeUnit unit)
+            throws InterruptedException, ExecutionException, TimeoutException {
+        if (unit == null)
+            throw new NullPointerException();
+        int s = state;
+        if (s <= COMPLETING &&
+                (s = awaitDone(true, unit.toNanos(timeout))) <= COMPLETING)
+            throw new TimeoutException();
+        return report(s);
+    }
+
+    /**
+     * Awaits completion or aborts on interrupt or timeout.
+     *
+     * @param timed true if use timed waits
+     * @param nanos time to wait, if timed
+     * @return state upon completion
+     */
+    private int awaitDone(boolean timed, long nanos)
+            throws InterruptedException {
+        final long deadline = timed ? System.nanoTime() + nanos : 0L;
+        WaitNode q = null;
+        boolean queued = false;
+        for (;;) {
+            if (Thread.interrupted()) {
+                removeWaiter(q);
+                throw new InterruptedException();
+            }
+
+            int s = state;
+            /**
+             * 已得到计算结果
+             */
+            if (s > COMPLETING) {
+                if (q != null)
+                    q.thread = null;
+                return s;
+            }
+            /**
+             * 正在结束的过程中，所以需要继续等待。
+             * 这里的方案也给我们编程一点启发，当线程需要等待一会才能决定下一步逻辑时：
+             * 1.在可预见的短周期内可得到结果，采用自旋的方式
+             * 2.可能稍长的时间才能得到结果，采用Thread.yield()
+             * 3.在比较长的时间才能得到结果，且需要线程同步的情况，采用wait(),notify()
+             */
+            else if (s == COMPLETING) // cannot time out yet
+                Thread.yield();
+            /**
+             * 将当前线程加入等待队列中
+             */
+            else if (q == null)
+                q = new WaitNode();
+            else if (!queued)
+                //相当于向链表头插入新的WaitNode，看起来就跟栈一样
+                queued = UNSAFE.compareAndSwapObject(this, waitersOffset,
+                        q.next = waiters, q);
+            /**
+             * 查看是否等待超时
+             */
+            else if (timed) {
+                nanos = deadline - System.nanoTime();
+                if (nanos <= 0L) {
+                    removeWaiter(q);
+                    return state;
+                }
+                LockSupport.parkNanos(this, nanos);
+            }
+            else
+                LockSupport.park(this);
+        }
+    }
+
+    /**
+     * Tries to unlink a timed-out or interrupted wait node to avoid
+     * accumulating garbage.  Internal nodes are simply unspliced
+     * without CAS since it is harmless if they are traversed anyway
+     * by releasers.  To avoid effects of unsplicing from already
+     * removed nodes, the list is retraversed in case of an apparent
+     * race.  This is slow when there are a lot of nodes, but we don't
+     * expect lists to be long enough to outweigh higher-overhead
+     * schemes.
+     */
+    private void removeWaiter(WaitNode node) {
+        //通过node.thread=null标记node
+        if (node != null) {
+            node.thread = null;
+            //将所有thread = null的node从队列中清除
+            retry:
+            for (;;) {          // restart on removeWaiter race
+                for (WaitNode pred = null, q = waiters, s; q != null; q = s) {
+                    //pred -> q -> s
+                    s = q.next;
+                    //q.thread != null，向后移
+                    if (q.thread != null)
+                        pred = q;
+                    //找到了为null的node 通过pred.next = s将其从队列中移除
+                    else if (pred != null) {
+                        pred.next = s;
+                        if (pred.thread == null) // check for race
+                            continue retry;
+                    }
+                    //pred == null && p.thread == null 将要被移除的node为队列头，通过CAS搞定
+                    else if (!UNSAFE.compareAndSwapObject(this, waitersOffset,
+                            q, s))
+                        continue retry;
+                }
+                break;
+            }
+        }
+    }
+
+    /**
+     * Returns result or throws exception for completed task.
+     * 请注意这里的返回优先级：
+     * 如果一个FutureTask既在run的时候抛出了exception，又调用了其cancell方法，则会抛出CancellationException，原来的ExecutionException将被吃掉！
+     * @param s completed state value
+     */
+    @SuppressWarnings("unchecked")
+    private V report(int s) throws ExecutionException {
+        Object x = outcome;
+        if (s == NORMAL)
+            return (V)x;
+        if (s >= CANCELLED)
+            throw new CancellationException();
+        throw new ExecutionException((Throwable)x);
     }
 }
